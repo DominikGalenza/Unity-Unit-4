f9db60e52abe26046f0fac56292bfb0b20c63fc6
Dominik Gałenza <dominik.galenza@gmail.com>
2022-04-21 15:42:52 +0200

    Added spawning enemy at random position

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    private float spawnRange = 9f;
+
+    void Start()
+    {
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 spawnPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        return spawnPos;
+    }
+}
